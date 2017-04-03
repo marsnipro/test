0a51c937b141ff791ddf1a5669e5b54b454bea39
marsnipro <marsnipro@gmail.com>
2017-04-03 14:56:43 +0900

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'AyKHjpRDJ1X0V0ahTbLZlLtqjQS6i5P9dkF9z6zObq4e4TmnKG46ZJyLk49YpuIBWbPTcY32SDsQrXPoKnRnkZPN8lWHF+9rPYS3VlnooRVehRAyE7vR0QtpDO0hkhTUufQKZzYnfZoUmjO0Y/lG0AdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
