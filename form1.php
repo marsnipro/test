572cef6554f9dbbe4f6af0505eb5fc762380ec8c
marsnipro <marsnipro@gmail.com>
2017-04-10 15:48:45 +0900

    Update form1.php

--- form1.php
+++ form1.php
@@ -31,14 +31,8 @@ function test_input($data) {
 ?>
 
 <h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
- 
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
