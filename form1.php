0f66eb8fc8e2ce9474256168affc5a8f31770289
marsnipro <marsnipro@gmail.com>
2017-04-10 16:03:45 +0900

    Update form1.php

--- form1.php
+++ form1.php
@@ -26,7 +26,7 @@ function test_input($data) {
 
 <h2>PHP Form Validation Example</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-   Name: <input type="text" name="name" value="$name">
+   Name: <input type="text" name="name" value="">
   <input type="submit" name="submit" value="Submit">  
 </form>
 
