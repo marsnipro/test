fdc6ea237631c4c5497d62d79cc6cc6981b97871
marsnipro <marsnipro@gmail.com>
2017-04-10 15:52:24 +0900

    Update form1.php

--- form1.php
+++ form1.php
@@ -9,19 +9,13 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+
+$name =  "";
+  
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
+  $name = test_input($_POST["name"]);
 }
+  
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
