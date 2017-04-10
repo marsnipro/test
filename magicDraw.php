6544747f3272bbd82556047777f1551d6a10b150
marsnipro <marsnipro@gmail.com>
2017-04-10 16:46:13 +0900

    Create magicDraw.php

---
+++ magicDraw.php
@@ -0,0 +1,21 @@
+<?php
+function point($fillColor, $backgroundColor) {
+
+    $draw = new \ImagickDraw();
+
+    $draw->setFillColor($fillColor);
+
+    for ($x = 0; $x < 10000; $x++) {
+        $draw->point(rand(0, 500), rand(0, 500));
+    }
+
+    $imagick = new \Imagick();
+    $imagick->newImage(500, 500, $backgroundColor);
+    $imagick->setImageFormat("png");
+    $imagick->drawImage($draw);
+
+    header("Content-Type: image/png");
+    echo $imagick->getImageBlob();
+}
+
+?>
